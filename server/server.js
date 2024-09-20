ed7f38e82764b16d7bb47b0feac79172a731c76d
Nirajan <nirajanshrestha672@gmail.com>
2024-09-20 01:03:41 -0400

    changed cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://expensyncapp.onrender.com",
     credentials: true,
   })
 );
